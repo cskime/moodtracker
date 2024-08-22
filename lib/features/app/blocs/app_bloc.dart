aff8872abbab4b87e60564d02fbac6339194d2a3
Chamsol Kim <kcsol1005@gmail.com>
2024-08-22 22:40:56 +0900

    refactor: AppBloc -> AppCubit

--- lib/features/app/blocs/app_bloc.dart
+++ lib/features/app/blocs/app_bloc.dart
@@ -4,30 +4,26 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:moodtracker/features/authentication/domain/entities/app_user.dart';
 import 'package:moodtracker/features/authentication/domain/repositories/auth_repository.dart';
 
-part 'app_event.dart';
 part 'app_state.dart';
 
-class AppBloc extends Bloc<AppEvent, AppState> {
+class AppBloc extends Cubit<AppState> {
   AppBloc({required this.authRepository})
       : super(
           authRepository.currentUser.isEmpty
               ? AppState.unauthenticated()
               : AppState.authenticated(user: authRepository.currentUser),
         ) {
-    on<AppUserChanged>(_onUserChanged);
-    _userSubscription = authRepository.user.listen(
-      (user) => add(AppUserChanged(user)),
-    );
+    _userSubscription = authRepository.user.listen(_onUserChanged);
   }
 
   final AuthRepository authRepository;
   StreamSubscription? _userSubscription;
 
-  void _onUserChanged(AppUserChanged event, Emitter<AppState> emit) {
+  void _onUserChanged(AppUser user) {
     emit(
-      event.user.isEmpty
+      user.isEmpty
           ? AppState.unauthenticated()
-          : AppState.authenticated(user: event.user),
+          : AppState.authenticated(user: user),
     );
   }
 
